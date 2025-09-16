bbb216be9efaabe12673991fe9975a062d4c0120
Zachariah Kersey <zrun2008@gmail.com>
2025-09-16 11:25:29 -0400

    Updated CodeBlock.jsx to show when content is copied, fixed authpage label color

--- frontend/src/components/CodeBlock.jsx
+++ frontend/src/components/CodeBlock.jsx
@@ -1,11 +1,12 @@
 import hljs from 'highlight.js';
 import 'highlight.js/styles/base16/classic-dark.css';
-import styles from "./CodeBlock.module.css";
-import { IoClipboardOutline } from "react-icons/io5";
-import { useEffect, useRef } from "react";
+import styles from "./codeBlock.module.css";
+import { IoClipboardOutline, IoCheckmarkCircleOutline } from "react-icons/io5";
+import { useEffect, useRef, useState } from "react";
 
 export default function CodeBlock({ code, language }) {
     const codeRef = useRef(null);
+    const [copied, setCopied] = useState(false);
     useEffect(() => {
         if (codeRef.current) {
             codeRef.current.textContent = code;
@@ -14,12 +15,22 @@ export default function CodeBlock({ code, language }) {
         }
     }, [code, language]);
 
+    const handleCopy = () => {
+        navigator.clipboard.writeText(code);
+        setCopied(true);
+        setTimeout(() => setCopied(false), 3000);
+    }
+
     return (
         <div className={styles.container}>
             <pre className={styles.pre}>
                 <code ref={codeRef} className={`language-${language} ${styles.code}`} />
             </pre>
-            <IoClipboardOutline className={styles.clipboardIcon} onClick={() => navigator.clipboard.writeText(code)} />
+            {copied ? (
+                <IoCheckmarkCircleOutline className={styles.clipboardIcon} onClick={handleCopy} />
+            ) : (
+                <IoClipboardOutline className={styles.clipboardIcon} onClick={handleCopy} />
+            )}
         </div>
     )
 }
