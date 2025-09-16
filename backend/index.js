b4695b9f6f29030b0e66f61d52db43a2a7890988
Zachariah Kersey <zrun2008@gmail.com>
2025-09-16 11:22:56 -0400

    Added mongodb to backend dependencies, created test for mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { MongoClient } = require('mongodb');
 
 dotenv.config();
 
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@formable.ruv01bs.mongodb.net/?retryWrites=true&w=majority&appName=Formable`;
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -20,4 +22,26 @@ app.listen(port, () => {
 
 app.get('/f/:id', (req, res) => {
   res.json({ message: req.params.id });
+});
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const people = await client.db("Formable").collection("Users").find({}).toArray();
+    return people;
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+app.get('/name', async (req, res) => {
+  const peopleList = await run();
+  console.log(peopleList);
+  res.json({ people: peopleList });
 });
